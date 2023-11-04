82bcfacf255785fa96bb38bdec9ec79eb704b3bb
naymurabir <naymurrahman.abir@gmail.com>
2023-11-04 17:20:09 +0600

    final

--- index.js
+++ index.js
@@ -78,28 +78,9 @@ async function run() {
         })
 
         //Remove token after logout the user
-        // app.post('/logout', async (req, res) => {
-        //     const user = req.body
-        //     console.log("User: ", user);
-        //     res.clearCookie('token', { maxAge: 0 }).send({ success: true })
-        // })
         app.post('/logout', async (req, res) => {
-            const user = req.body;
-            console.log('logging out', user);
-            res.clearCookie(
-                "token",
-                {
-                    maxAge: 0,
-                    // secure: process.env.NODE_ENV === "production" ? true : false,
-                    // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-                }
-            ).send({ success: true })
-        })
-
-        // Remove token by JWT
-        app.post('/logout', (req, res) => {
             const user = req.body
-            console.log("Logging out", user);
+            console.log("User: ", user);
             res.clearCookie('token', { maxAge: 0 }).send({ success: true })
         })
 
